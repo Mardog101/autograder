55ac3098a27966686a62c038dbdd2303726bd9de
Marco De Vincentiis <marco.devincentiis@gmail.com>
2023-12-06 21:25:09 +0100

    Added -i shows input of failed test cases

--- autograder.sh
+++ autograder.sh
@@ -6,18 +6,20 @@ execcmd="./"
 execauto=true
 testfolder="tests"
 verbose=false
+showinput=false
 
 colorSuccess='\033[0;32m'
 colorTimeout='\033[0;33m'
 colorFailure='\033[0;31m'
 colorRemove='\033[0m'
 
-while getopts 't:p:d:ve:' flag; do
+while getopts 't:p:d:vie:' flag; do
   case "${flag}" in
     t) time=${OPTARG} ;;
     p) execfile="${OPTARG}" ;;
     d) testfolder="${OPTARG}" ;;
     v) verbose=true ;;
+    i) showinput=true ;;
     e) execcmd="${OPTARG}"
        execauto=false;;
   esac
@@ -58,6 +60,10 @@ for file in $testfolder/*.in ; do
   elif [[ $(diff $name.out $name.out.temp) ]]; then
     echo -e "${colorFailure}INCORRECT OUTPUT : $name${colorRemove}"
     if [ $verbose = true ]; then
+      if [ $showinput = true ]; then
+        echo -e "${colorFailure}---========= Input ============---${colorRemove}"
+        cat $name.in
+      fi
       echo -e "${colorFailure}---========= Expected =========---${colorRemove}"
       cat $name.out
       echo -e "${colorFailure}---========= Produced =========---${colorRemove}"
