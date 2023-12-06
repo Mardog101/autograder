e86225ad58bb6b42e24b5660e04f990f21bb784f
Marco De Vincentiis <marco.devincentiis@gmail.com>
2023-12-06 14:54:18 +0100

    Added -e execute method flag and python support.

--- autograder.sh
+++ autograder.sh
@@ -2,20 +2,24 @@
 
 time=1
 execfile="a.out"
+execcmd="./"
+execauto=true
 testfolder="tests"
-verbose= false
+verbose=false
 
 colorSuccess='\033[0;32m'
 colorTimeout='\033[0;33m'
 colorFailure='\033[0;31m'
 colorRemove='\033[0m'
 
-while getopts 't:p:d:v' flag; do
+while getopts 't:p:d:ve:' flag; do
   case "${flag}" in
     t) time=${OPTARG} ;;
     p) execfile="${OPTARG}" ;;
     d) testfolder="${OPTARG}" ;;
-    v) verbose= true ;;
+    v) verbose=true ;;
+    e) execcmd="${OPTARG}"
+       execauto=false;;
   esac
 done
 
@@ -37,18 +41,29 @@ for file in $testfolder/*.in ; do
   fi
 done
 
+if [ $execauto = true ]; then
+  case ${execfile##*.} in
+    out)
+      execcmd="./";;
+    py)
+      execcmd="python ";;
+  esac
+fi
+
 for file in $testfolder/*.in ; do
   name=${file%.in}
-  timeout $time ./$execfile < $file > $name.out.temp
+  timeout $time $execcmd$execfile < $file > $name.out.temp
   if [[ $? -eq 124 ]]; then
     echo -e "${colorTimeout}TIMEOUT : $name${colorRemove}"
   elif [[ $(diff $name.out $name.out.temp) ]]; then
-    echo -e "${colorFailure}INCORRECT OUTPUT : $name}"
-    echo -e "---========= Expected =========---${colorRemove}"
-    cat $name.out
-    echo -e "${colorFailure}---========= Produced =========---${colorRemove}"
-    cat $name.out.temp
-    echo -e "${colorFailure}---============================---${colorRemove}"
+    echo -e "${colorFailure}INCORRECT OUTPUT : $name${colorRemove}"
+    if [ $verbose = true ]; then
+      echo -e "${colorFailure}---========= Expected =========---${colorRemove}"
+      cat $name.out
+      echo -e "${colorFailure}---========= Produced =========---${colorRemove}"
+      cat $name.out.temp
+      echo -e "${colorFailure}---============================---${colorRemove}"
+    fi
   else
     echo -e "${colorSuccess}PASSED : $name${colorRemove}"
   fi
